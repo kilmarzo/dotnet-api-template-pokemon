7fb5aa3863c7b746b058217d7455b0a75bd7f901
selka <96589955+kilmarzo@users.noreply.github.com>
2023-07-10 11:02:53 +0200

    Changed the standard Get method to include some advice with each different type of weather and also changed the DateOnly to DateTime

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -6,10 +6,20 @@ namespace dotnet_api_template.Controllers;
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+    private static readonly Dictionary<string, string> Summaries = new Dictionary<string, string>()
+{
+    {"Freezing", "Better stay inside, it's freezing!"},
+    {"Bracing", "Brace yourself, it's quite cold!"},
+    {"Chilly", "It's chilly, don't forget your coat."},
+    {"Cool", "It's cool, you might need a light sweater."},
+    {"Mild", "It's mild, dress normally."},
+    {"Warm", "It's warm, enjoy the nice weather!"},
+    {"Balmy", "It's balmy, feel the breeze."},
+    {"Hot", "It's hot, stay hydrated."},
+    {"Sweltering", "It's sweltering, better find some shade."},
+    {"Scorching", "It's scorching, make sure to wear sunscreen."},
+};
+
 
     private readonly ILogger<WeatherForecastController> _logger;
 
@@ -21,14 +31,20 @@ public class WeatherForecastController : ControllerBase
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        return Enumerable.Range(1, 5).Select(index =>
         {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            var summaryKey = Summaries.Keys.ElementAt(Random.Shared.Next(Summaries.Count));
+            return new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = summaryKey,
+                Advice = Summaries[summaryKey]
+            };
         })
         .ToArray();
     }
+
 
     [HttpGet("{daysAhead:int}", Name = "GetSpecificDayWeatherForecast")]
     public WeatherForecast Get(int daysAhead)
--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -2,11 +2,13 @@ namespace dotnet_api_template;
 
 public class WeatherForecast
 {
-    public DateOnly Date { get; set; }
+    public DateTime Date { get; set; }
 
     public int TemperatureC { get; set; }
 
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
     public string? Summary { get; set; }
+    
+    public string Advice { get; set; }
 }
